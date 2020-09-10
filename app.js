ad8154574f4dbee88a3e0a4b63bc7c8b39b0d483
arana4127 <arana4127@gmail.com>
2020-09-10 20:40:19 +0530

    Added custom expiry time

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const app=express();
+const app = express();
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -9,49 +9,62 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/boardinfinityDB", {
   useUnifiedTopology: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useCreateIndex: true
 });
 
-const TodoSchema =new mongoose.Schema({
-  taskName:String,
-  description:String,
-  creator:String,
-  duration:Number,
-  createdAt:Date,
-  // expire_at: {type: Date, default: createdAt, expires: duration}
+
+/////////////////////////////////////////////Defining Schema//////////////////////////////////////////////////
+
+const TodoSchema = new mongoose.Schema({
+  taskName: String,
+  description: String,
+  creator: String,
+  duration: Number,
+  createdAt: {
+    type: Date,
+    expires: 0
+  }
 });
 
-const Task=mongoose.model("Task",TodoSchema);
 
-app.get('/list',function(req,res){
-  Task.find(function(err,task){
-    if(!err){
+const Task = mongoose.model("Task", TodoSchema);
+
+//////////////////////////////////////////Routes////////////////////////////////////////////////////////////////
+app.get('/list', function(req, res) {
+  Task.find(function(err, task) {
+    if (!err) {
       res.send(task);
-    }
-    else{
+    } else {
       res.send(err);
     }
   });
 });
 
-app.post('/add',function(req,res){
-  const newTask=new Task({
-    taskName:req.body.task,
-    description:req.body.description,
-    creator:req.body.creator,
-    duration:req.body.duration,
-    createdAt:new Date()
+app.post('/add', function(req, res) {
+
+  var timeObject = new Date();
+  var seconds = timeObject.getSeconds() + req.body.duration;
+  timeObject = timeObject + seconds;
+  const newTask = new Task({
+    taskName: req.body.task,
+    description: req.body.description,
+    creator: req.body.creator,
+    duration: req.body.duration,
+    createdAt: timeObject
   });
-  newTask.save(function(err){
-    if(!err){
+  newTask.save(function(err) {
+    if (!err) {
       res.send("Successfully added a new Task");
+    } else {
+      console.log(err);
     }
   });
 });
 
 
+//////////////////////////////////////////////End of Routes//////////////////////////////////////////////////////
 
-
-app.listen(process.env.PORT||3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
